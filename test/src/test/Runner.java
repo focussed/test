0d276b9314fef3a9e8393267cd3b4f1ebde3476d
Ian Craig <craig.ian@itsligo.ie>
2016-03-15 15:14:06 +0000

    initial

---
+++ test/src/test/Runner.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author icraig2
+ *
+ */
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+
+	}
+
+}
